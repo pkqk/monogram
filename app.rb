807ba0dbefba7e9382aa0d86bcef6e0a70be370b
Adam Sven Johnson <adam@pkqk.net>
2013-07-17 16:22:51 +0100

    Be more layout

--- app.rb
+++ app.rb
@@ -11,15 +11,7 @@ end
 
 post '/' do
   img = params[:img]
-  html = <<HTML
-<html>
-<style>
-body { margin; 0; padding: 0; }
-img { padding: 16px; border: 4px dashed black; margin: 21px; }
-</style>
-<img src="#{img}">
-</html>
-HTML
+  html = erb :polaroid, :locals => { :img => img }
   RestClient.post(direct_print, :html => html)
   status 204
   ''
